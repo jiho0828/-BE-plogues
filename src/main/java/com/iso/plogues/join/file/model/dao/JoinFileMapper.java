94c3823caed124640210041d3e4336589ac88230
grape-fruit-apricot <4.fear.1ess.4ra@gmail.com>
2026-06-30 15:36:42 +0900

    feat:플로깅, 식목 게시판 전체조회 구현

--- src/main/java/com/iso/plogues/join/file/model/dao/JoinFileMapper.java
+++ src/main/java/com/iso/plogues/join/file/model/dao/JoinFileMapper.java
@@ -1,5 +1,14 @@
 package com.iso.plogues.join.file.model.dao;
 
+<<<<<<< Updated upstream
+=======
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+import com.iso.plogues.template.file.File;
+
+@Mapper
+>>>>>>> Stashed changes
 public interface JoinFileMapper {
 
 }
--- src/main/java/com/iso/plogues/join/file/model/service/JoinFileService.java
+++ src/main/java/com/iso/plogues/join/file/model/service/JoinFileService.java
@@ -1,5 +1,18 @@
 package com.iso.plogues.join.file.model.service;
 
+<<<<<<< Updated upstream
+=======
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.iso.plogues.join.file.model.dao.JoinFileMapper;
+import com.iso.plogues.template.file.File;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+>>>>>>> Stashed changes
 public class JoinFileService {
 
 }
