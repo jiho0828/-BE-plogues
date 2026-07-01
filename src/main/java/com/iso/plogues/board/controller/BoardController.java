753ef2e03fe5fa741894094be3de516b2fb1e9e1
ham-zi <ddmt0805@gmail.com>
2026-07-01 11:17:19 +0900

    import

--- src/main/java/com/iso/plogues/board/controller/BoardController.java
+++ src/main/java/com/iso/plogues/board/controller/BoardController.java
@@ -1,5 +1,6 @@
 package com.iso.plogues.board.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
