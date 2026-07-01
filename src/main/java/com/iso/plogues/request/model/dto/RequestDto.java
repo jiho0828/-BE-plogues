28ef9b8de73b6b74a4ca20ee3911c046e66a4de1
ham-zi <ddmt0805@gmail.com>
2026-07-01 11:51:33 +0900

    @size(200)추가

--- src/main/java/com/iso/plogues/request/model/dto/RequestDto.java
+++ src/main/java/com/iso/plogues/request/model/dto/RequestDto.java
@@ -1,5 +1,5 @@
 package com.iso.plogues.request.model.dto;
-
+import jakarta.validation.constraints.Size;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -16,7 +16,8 @@ public class RequestDto {
 	private Long joinRequestNo;
 	private Long joinNo;
 	private String userId;
-	@NotBlank(max=200 ,message="포부를 작성해주세요.")
+	@Size(max = 200, message = "포부는 200자 이하로 작성해주세요.")
+	@NotBlank(message="포부를 작성해주세요.")
 	private String aspiration;
 	private String status;
 }
