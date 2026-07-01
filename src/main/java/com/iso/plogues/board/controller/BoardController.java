47e0cb5d0900a0df5fa30384b45389a168d2e5fb
jiho0828 <njiho0828@gmail.com>
2026-07-01 16:25:38 +0900

    인서트2

--- src/main/java/com/iso/plogues/board/controller/BoardController.java
+++ src/main/java/com/iso/plogues/board/controller/BoardController.java
@@ -9,13 +9,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
-<<<<<<< HEAD
 import org.springframework.web.multipart.MultipartFile;
-=======
 import org.springframework.http.ResponseEntity;
->>>>>>> e37315da74b60e447fa535184659b072ba949b0a
 
 import com.iso.plogues.api.model.vo.ApiResponse;
 import com.iso.plogues.auth.model.vo.CustomUserDetails;
